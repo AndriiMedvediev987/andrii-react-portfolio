964fa263ca41be051e8a9eb60a05594dbb9baf59
Andrii Medvediev <42775640+Andrew1Medvedev@users.noreply.github.com>
2024-04-15 10:11:11 +1000

    Case sensitive fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Navigator from './components/Navigator';
-import Footer from './components/Footer';
+import Navigator from './Components/Navigator';
+import Footer from './Components/Footer';
 
 function App() {
   return (
