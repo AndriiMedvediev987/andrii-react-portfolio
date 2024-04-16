844c146a36c53589e24d0f75f0903b7c10fb6f88
AndriiMedvediev987 <144401796+AndriiMedvediev987@users.noreply.github.com>
2024-04-16 11:44:53 +1000

    Update works.js

--- src/works.js
+++ src/works.js
@@ -7,6 +7,31 @@ import image6 from './assets/images/VisualTheme_preview.png';
 
 const works = [
   {
+    link: "https://github.com/AndriiMedvediev987/andrii-social-network",
+    image: "https://github.com/AndriiMedvediev987/andrii-social-network/assets/144401796/33f240f0-8ea1-46d2-a2fb-f6dd5be2bccc",
+    name: "Social network"
+  },
+  {
+    link: "https://github.com/AndriiMedvediev987/andrii-tech-blog",
+    image: "https://github.com/AndriiMedvediev987/andrii-tech-blog/assets/144401796/07aa580e-e5a8-42fb-940c-692723ea5803",
+    name: "Tech blog"
+  },
+  {
+    link: "https://github.com/AndriiMedvediev987/andrii_codequiz",
+    image: "https://github.com/AndriiMedvediev987/andrii_codequiz/assets/144401796/0fac7e99-2e36-42f1-8c4e-6bc19adc4577",
+    name: "Code quiz"
+  },
+  {
+    link: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard",
+    image: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard/assets/144401796/c7ed1390-b68f-4b72-9a43-800ad436a5f0",
+    name: "Weather dashboard"
+  },
+  {
+    link: "https://github.com/AndriiMedvediev987/andrii-calendar",
+    image: "https://github.com/AndriiMedvediev987/andrii-calendar/assets/144401796/7c688583-b324-469b-9d30-afa1fde333b1",
+    name: "Calendar"
+  },
+  {
     link: "https://www.alternetsoft.com/demos",
     src: image1,
     image: "../assets/images/AlternetStudio_preview.png",
@@ -41,26 +66,6 @@ const works = [
     src: image6,
     image: "../assets/assets/images/VisualTheme_preview.png",
     name: "Visual theme"
-  },
-  {
-    link: "https://github.com/AndriiMedvediev987/andrii_codequiz",
-    image: "https://github.com/AndriiMedvediev987/andrii_codequiz/assets/144401796/0fac7e99-2e36-42f1-8c4e-6bc19adc4577",
-    name: "Code quiz"
-  },
-  {
-    link: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard",
-    image: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard/assets/144401796/c7ed1390-b68f-4b72-9a43-800ad436a5f0",
-    name: "Weather dashboard"
-  },
-  {
-    link: "https://github.com/AndriiMedvediev987/andrii-calendar",
-    image: "https://github.com/AndriiMedvediev987/andrii-calendar/assets/144401796/7c688583-b324-469b-9d30-afa1fde333b1",
-    name: "Calendar"
-  },
-  {
-    link: "https://github.com/AndriiMedvediev987/andrii-social-network",
-    image: "https://github.com/AndriiMedvediev987/andrii-social-network/assets/144401796/33f240f0-8ea1-46d2-a2fb-f6dd5be2bccc",
-    name: "Social network"
   },
 ];
 
