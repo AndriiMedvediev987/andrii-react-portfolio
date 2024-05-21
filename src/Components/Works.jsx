246a58f81fbd84b0baa64be4edc7721bfc062eec
Andrii Medvediev <42775640+Andrew1Medvedev@users.noreply.github.com>
2024-05-21 19:03:00 +1000

    change work page

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -14,6 +14,7 @@ function Work(props) {
             {props.works.map((work) => (
                 <div key={work.name} className="work">
                     <h3>{work.name}</h3>
+                    <h5>{work.description}</h5>
                     <div>
                         {work.link ? (
                             <a href={work.link} target="_blank" rel="noopener noreferrer">
@@ -23,6 +24,10 @@ function Work(props) {
                             img(work)
                         )}
                     </div>
+                    <div style={{ color: 'darkgray' }}>Used skills or technologies: <div style={{ display: 'inline-block', color: 'black' }}>{work.skills}</div></div>
+                    {work.deploy ? <div>
+                        <div style={{ color: 'darkgray' }}>Deploy here: <a href={work.deploy} target="_blank" rel="noopener noreferrer">{work.name}</a></div>
+                    </div> : <div/>}
                 </div>
             ))}
         </div>
--- src/works.js
+++ src/works.js
@@ -9,63 +9,96 @@ const works = [
   {
     link: "https://github.com/AndriiMedvediev987/andrii-social-network",
     image: "https://github.com/AndriiMedvediev987/andrii-social-network/assets/144401796/33f240f0-8ea1-46d2-a2fb-f6dd5be2bccc",
-    name: "Social network"
+    name: "Social network",
+    description: "This project represents an social network (only Back End) application allows user to provide CRUD operations with socialnetworkDB database",
+    deploy: "https://github-production-user-asset-6210df.s3.amazonaws.com/144401796/318385885-825ea71f-35f0-4e63-a286-17dc884b0fe4.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240521%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240521T085816Z&X-Amz-Expires=300&X-Amz-Signature=943d6b1400ba471b887b3bc5ea88cac47162ecaa3403c962e3081db3d0875653&X-Amz-SignedHeaders=host&actor_id=144401796&key_id=0&repo_id=778644543",
+    skills: "Express, Mongoose and MongoDB."
   },
   {
     link: "https://github.com/AndriiMedvediev987/andrii-tech-blog",
     image: "https://github.com/AndriiMedvediev987/andrii-tech-blog/assets/144401796/07aa580e-e5a8-42fb-940c-692723ea5803",
-    name: "Tech blog"
+    name: "Tech blog",
+    description: "This project represents a Blog application allows user to create new post, and comment existing. Users can view all posts, comment on them, and create new ones after logging in.",
+    deploy: "https://andrii-tech-blog-2178d1cc774b.herokuapp.com/",
+    skills: "Inquirer, MySQL, Handlebars and Sequelize."
   },
   {
     link: "https://github.com/AndriiMedvediev987/andrii_codequiz",
     image: "https://github.com/AndriiMedvediev987/andrii_codequiz/assets/144401796/0fac7e99-2e36-42f1-8c4e-6bc19adc4577",
-    name: "Code quiz"
+    name: "Code quiz",
+    description: "This project represents a code quiz with several questions. It allows the user to check his knowledge about JavaScript.",
+    deploy: "https://andriimedvediev987.github.io/andrii_codequiz/",
+    skills: "JavaScript and localStorage."
   },
   {
     link: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard",
     image: "https://github.com/AndriiMedvediev987/andrii-weather-dashboard/assets/144401796/c7ed1390-b68f-4b72-9a43-800ad436a5f0",
-    name: "Weather dashboard"
+    name: "Weather dashboard",
+    description: "This project represents a weather dashboard with the ability to store previously asked cities. It allows the user to receive a weather forecast for the next 5 days.",
+    deploy: "https://andriimedvediev987.github.io/andrii-weather-dashboard/",
+    skills: "OpenWeatherMap API and localStorage."
   },
   {
     link: "https://github.com/AndriiMedvediev987/andrii-calendar",
     image: "https://github.com/AndriiMedvediev987/andrii-calendar/assets/144401796/7c688583-b324-469b-9d30-afa1fde333b1",
-    name: "Calendar"
+    name: "Calendar",
+    description: "This project represents a simple calendar used to add, edit and store appointments for the current day.",
+    deploy: "https://andriimedvediev987.github.io/andrii-calendar/",
+    skills: "JavaScript, jQuery, Day.js and localStorage."
   },
   {
     link: "https://www.alternetsoft.com/demos",
     src: image1,
     image: "../assets/images/AlternetStudio_preview.png",
-    name: "Studio"
+    name: "Studio",
+    description: "AlterNET Studio Demo shows how our components work together, from designing visual interfaces using Form Designer to writing application logic in Code Editor and executing and debugging this code with Scripter.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
   {
     link: "",
     src: image2,
     image: "../assets/images/CodeEditorSyntax_preview.png",
-    name: "Code Editor"
+    name: "Code Editor",
+    description: "Code Editor Demo shows the main features and capabilities of our text edit control.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
   {
     link: "",
     src: image3,
     image: "../assets/images/DebuggerIntegration_preview.png",
-    name: "Debugger integration"
+    name: "Debugger integration",
+    description: "This demo shows how script debugger control can be integrated into the application.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
   {
     link: "",
     src: image4,
     image: "../assets/images/FormDesigner_preview.png",
-    name: "Form designer"
+    name: "Form designer",
+    description: "This projects shows how to build visual interfaces by placing controls to the design surface, arrange them and write code for their events.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
   {
     link: "",
     src: image5,
     image: "../assets/images/SnippetsParsers_preview.png",
-    name: "Snippet parsers"
+    name: "Snippet parsers",
+    description: "The demo project shows how to use \"partial code\" parsers.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
   {
     link: "",
     src: image6,
     image: "../assets/assets/images/VisualTheme_preview.png",
-    name: "Visual theme"
+    name: "Visual theme",
+    description: "Visual theme control appearance of all editor elements, using a predefined set of fonts and colors.",
+    deploy: "",
+    skills: "Visual Studio, WinForms, C#, Roslyn."
   },
 ];
 
